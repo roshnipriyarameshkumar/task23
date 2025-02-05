96dfa84f9c25348d8d6a228d7b106e96f0bc727e
Roshnipriya <127740197+roshnipriyarameshkumar@users.noreply.github.com>
2025-02-05 11:32:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u roshni178-p Foxowlviya@21
+    docker login -u roshni178 -p Foxowlviya@21
     docker tag test roshni178/task
     docker push roshni178/task
     
